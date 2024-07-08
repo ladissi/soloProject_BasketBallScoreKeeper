5e166eee7e9fdffe3e02e64c997933df9a19bcd1
Levi Adissi <leviadissi@gmail.com>
2024-07-08 10:55:18 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -23,5 +23,35 @@ function updateGuestScore(val) {
 
 function resetGuest() {
     document.getElementById("guestScore").textContent = 0;
-
 }
+
+function updateGreen() {
+    document.body.style.background = "#064E3B";
+    document.documentElement.style.background = "#064E3B";
+}
+
+function updateBlue() {
+    document.body.style.background = "#0C4A6E";
+    document.documentElement.style.background = "#0C4A6E";
+    }
+
+
+function updateRed() {
+    document.body.style.background = "#881337";
+    document.documentElement.style.background = "#881337";
+}
+
+function updateRandom() {
+    document.body.style.background = getRandomColor();
+    document.documentElement.style.background = getRandomColor();
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+        }
+
